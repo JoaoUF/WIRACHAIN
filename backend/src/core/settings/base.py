2a58ab6177522fc31cbae3f1a646ae7cfaa04e84
Joao Urrunaga <u201913507@upc.edu.pe>
2025-10-25 08:28:25 -0500

    add session authentication && change order

--- backend/src/core/settings/base.py
+++ backend/src/core/settings/base.py
@@ -137,8 +137,9 @@ REST_FRAMEWORK = {
         "rest_framework.filters.OrderingFilter",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.TokenAuthentication",
         "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.AllowAny"],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
