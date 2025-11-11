b4677b97a1a8fde55a40b1ffb973f1e0fb212a46
Joao Urrunaga <u201913507@upc.edu.pe>
2025-11-11 09:32:25 -0500

    add fixtures of test, disease, speciality for base clinic

--- docker/backend/server-entrypoint.sh
+++ docker/backend/server-entrypoint.sh
@@ -39,7 +39,7 @@ done
 #     sleep 2
 # done
 
-until python manage.py loaddata groups.json users.json email_adresses.json
+until python manage.py loaddata groups.json users.json email_adresses.json test.json disease.json speciality.json
 do
     echo "Loading groups fixture..."
     sleep 2
